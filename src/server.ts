749f85fcfa2e81fa8cdba514f870324a851691e5
Kilson <kilsonrs@gmail.com>
2021-03-02 10:45:03 -0300

    chore: remove console.log

--- src/server.ts
+++ src/server.ts
@@ -2,7 +2,7 @@ import app from './app';
 import 'dotenv/config'
 
 const port = process.env.APP_API_PORT;
-console.log(process.env.APP_API_PORT)
+
 app.listen(port, () => {
   console.log('🚀 Server started on port 3334');
 });
